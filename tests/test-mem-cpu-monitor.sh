2bdf01de24b47fd989bb4195ab7ae4923f06d2ec
Eero Tamminen <eero.tamminen@nokia.com>
2010-02-01 14:38:08 +0000

    add test script for mem-cpu-monitor

---
+++ tests/test-mem-cpu-monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+log=mem-cpu-monitor.log
+
+exit_cleanup ()
+{
+	rm -f $log
+}
+trap exit_cleanup EXIT
+
+mem-cpu-monitor -i 1 --self > $log &
+pid=$!
+sleep 4
+kill -TERM $pid
+# error if no time output in log file
+grep -q '^[0-9]\+:[0-9]\+:[0-9]\+ ' $log
